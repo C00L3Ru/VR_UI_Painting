48853ac4dda59b203d35f98cefec4a35dd18fdf9
c00l3ru <jthornton973@gmail.com>
2019-08-11 08:53:23 +0100

    Initial commit

---
+++ Source/VRPaintingUI/VRPaintingUIGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRPaintingUIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPAINTINGUI_API AVRPaintingUIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
